33511b105fff9fe0729bf8a2bded011d3844a5e2
Beatrice <explaintotris@gmail.com>
2018-04-22 16:31:04 +0800

    compelte tutorial

---
+++ tutorial/options.js
@@ -0,0 +1,16 @@
+const kButtonColors = ['#18a5ad', '#771bbe', '#d08d47']
+const page = document.getElementById('button--container')
+
+function constructOptions(kButtonColors) {
+    for (let item of kButtonColors) {
+      let button = document.createElement('button');
+      button.style.backgroundColor = item;
+      button.addEventListener('click', function() {
+        chrome.storage.sync.set({color: item}, function() {
+          console.log('color is ' + item);
+        })
+      });
+      page.appendChild(button);
+    }
+  }
+  constructOptions(kButtonColors);
